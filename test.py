f48228e41d3518ab01e0a2fc446cd2b1148f1f39
Alexander Sferrella <alexander@sferrella.com>
2023-05-14 17:52:22 -0500

    I don't think pyrcrack actually does what I need

--- test.py
+++ test.py
@@ -1,12 +1,38 @@
+import asyncio
+
 import pyrcrack
 
-# so pycrack is just outright not supported on windows
-# and I couldn't make it work without rewriting the whole thing
-# on the pi it is then
+from rich.console import Console
 
-import os
-os.environ["SKIP_ROOT_CHECK"] = "1"
-print(os.getenv("SKIP_ROOT_CHECK"))
+from time import time
 
-airmon = pyrcrack.AirmonNg()
-print(airmon.interfaces)
+async def scan_for_targets(start, length):
+    """Scan for targets, return json."""
+    console = Console()
+    airmon = pyrcrack.AirmonNg()
+
+    interface = "wlan1"
+    for i in await airmon.interfaces:
+        print(f'Available: {i}')
+
+    async with airmon(interface) as mon:
+        async with pyrcrack.AirodumpNg() as pdump:
+            async for result in pdump(mon.monitor_interface):
+                #Essid, Bssid, Packets, Dbm, Score, Channel, Encryption
+                #console.print(result.table)
+                #print(result.table.columns[0].header) # 'Essid'
+                #print(result.table.rows)
+                print(result.table.columns)
+
+                '''
+                wtf? pyrcrack just actually doesn't let you configure anything
+                This is totally useless
+                Also only returning things in rich table format is pretty useless
+                '''
+
+                if time() - start  >= length:
+                    break
+
+print("Starting")
+asyncio.run(scan_for_targets(time(), 5))
+print("Done")
