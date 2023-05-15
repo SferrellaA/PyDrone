5fa46d28c22686eb840e3dd081340ee9440490a9
Alexander Sferrella <alexander@sferrella.com>
2023-05-14 21:27:17 -0500

    managed a drone telemetry capture

--- test.py
+++ test.py
@@ -1,38 +1,76 @@
 import asyncio
-
-import pyrcrack
-
-from rich.console import Console
+from os import listdir
 
 from time import time
 
-async def scan_for_targets(start, length):
-    """Scan for targets, return json."""
-    console = Console()
-    airmon = pyrcrack.AirmonNg()
 
-    interface = "wlan1"
-    for i in await airmon.interfaces:
-        print(f'Available: {i}')
+# list available interfaces
+print(listdir('/sys/class/net/'))
 
-    async with airmon(interface) as mon:
-        async with pyrcrack.AirodumpNg() as pdump:
-            async for result in pdump(mon.monitor_interface):
-                #Essid, Bssid, Packets, Dbm, Score, Channel, Encryption
-                #console.print(result.table)
-                #print(result.table.columns[0].header) # 'Essid'
-                #print(result.table.rows)
-                print(result.table.columns)
 
-                '''
-                wtf? pyrcrack just actually doesn't let you configure anything
-                This is totally useless
-                Also only returning things in rich table format is pretty useless
-                '''
+# sudo airodump-ng wlan1 --essid ATTBRyYu7C -w asdf --output-format cap
 
-                if time() - start  >= length:
-                    break
+'''
+import pyshark
+cap = pyshark.FileCapture('asdf-01.cap')
+for packet in cap:
+    try:
+        print(packet.data.data)
+        for x in packet:
+            print(x)
+    except:
+        pass
+'''
 
-print("Starting")
-asyncio.run(scan_for_targets(time(), 5))
-print("Done")
+import pyshark
+for packet in pyshark.FileCapture("WiFiUFO_211267.cap"):
+    if not hasattr(packet, 'ip'):
+        continue
+    #print(packet.layers)
+    '''
+    if hasattr(packet, "ip"):
+        print(packet.ip.src)
+        print(packet.ip.dst)
+    if hasattr(packet, 'udp'):
+        print(packet.udp.srcport)
+        print(packet.udp.dstport)
+    if hasattr(packet, "dns"):
+        print(packet.dns.name)
+    '''
+    #if hasattr(packet, 'data'):
+    #    print(packet.udp.payload)
+    if hasattr(packet, 'dns'):
+        print(packet.dns.qry_name)
+
+
+'''
+capture = pyshark.LiveCapture(interface='wlan1')
+for packet in capture.sniff_continuously(packet_count=5):
+    #if hasattr(packet, 'ip'):
+        #print(f"[{packet.ip.src} to {packet.ip.dst}]")
+        #print(packet.transport_layer)
+        #print(packet.highest_layer)
+    print(packet.layers)
+    #print(packet.WLAN)
+'''
+
+
+
+# Findings
+'''
+wifi name = WiFiUFO_21267
+no encryption, but the data sent is in hex anyway
+
+need to skip resent packets
+The drone itself is 192.168.28.1
+the attached phone is 192.168.28.2
+
+There are occasionally dns requests _from the phone_ for www.google.com and connectivitycheck.gstatic.com
+Crappy programming I guess
+
+commands (phone to drone) are 14 bytes (out of 76 byte packet length)
+video(?) (drone to phone) is variable in length but mostoften 1472/1534 bytes
+
+it would probably be useful save to new files each time you're trying a new action
+    alternatively take pictures between each action
+'''
